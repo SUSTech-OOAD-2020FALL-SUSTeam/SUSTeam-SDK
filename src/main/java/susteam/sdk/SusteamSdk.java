945dd5620e41456b9ee667d82eb32bf814a9ce11
yinpe <11810305@mail.sustech.edu.cn>
2020-11-24 22:47:50 +0800

    add Save

--- src/main/java/susteam/sdk/SusteamSdk.java
+++ src/main/java/susteam/sdk/SusteamSdk.java
@@ -4,37 +4,34 @@ import io.vertx.core.Future;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
 import io.vertx.core.buffer.Buffer;
+import io.vertx.core.file.FileSystem;
+import io.vertx.core.file.OpenOptions;
+import io.vertx.core.streams.ReadStream;
 import io.vertx.ext.web.client.HttpRequest;
 import io.vertx.ext.web.client.WebClient;
 import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.ext.web.multipart.MultipartForm;
 
 
 public class SusteamSdk {
 
-    public static final String SERVER = "susteam.gogo.moe";
+    public static final String SERVER_HOST = "susteam.gogo.moe";
 
     public static Vertx vertx;
     public static WebClient client;
     public static String token;
-    public static Game game;
+    public static int gameId;
 
     public static void init(String token, int gameId) {
         vertx = Vertx.vertx();
         client = WebClient.create(
                 vertx,
-                new WebClientOptions().setDefaultHost(SERVER)
+                new WebClientOptions().setDefaultHost(SERVER_HOST)
         );
         SusteamSdk.token = token;
-
-
-        Game(gameId).onSuccess( it -> {
-           SusteamSdk.game = it;
-        }).onFailure( it -> {
-
-        });
+        SusteamSdk.gameId = gameId;
     }
-
-    public static Future<Game> Game(int gameId) {
+    public static Future<Game> getGame(int gameId) {
         Promise<Game> promise = Promise.promise();
         HttpRequest<Buffer> request = client.get("/api/game/"+gameId);
         request.send(result -> {
@@ -43,8 +40,9 @@ public class SusteamSdk {
                 return;
             }
             //TODO can't get Chinese character
+            System.out.println(result.result().bodyAsJsonObject());
             if(result.result().bodyAsJsonObject().getBoolean("success")) {
-                game = GameKt.toGame(result.result().bodyAsJsonObject().getJsonObject("game"));
+                Game game = GameKt.toGame(result.result().bodyAsJsonObject().getJsonObject("game"));
                 promise.complete(game);
             }
             else {
@@ -92,58 +90,109 @@ public class SusteamSdk {
         return promise.future();
     }
 
-    public static Future<Void> uploadSave(String path) {
+
+    public static Future<Void> save(String path, String filename) {
+
         Promise<Void> promise = Promise.promise();
-        HttpRequest<Buffer> request = client.post("/api/save").bearerTokenAuthentication(token);
-        //TODO api
-        try {
-            vertx.fileSystem().readFile(path, readResult -> {
-                if(readResult.succeeded()) {
-                    request.sendBuffer(readResult.result(), result -> {
-                        if (result.failed()) {
-                            promise.fail(result.cause());
-                            return;
-                        }
-                        promise.complete();
-                    });
-                }
-                else {
-                    promise.fail(readResult.cause());
+        HttpRequest<Buffer> request = client.get("/api/token").bearerTokenAuthentication(token);
+        request.send(result -> {
+            if (result.failed()) {
+                promise.fail(result.cause());
+                return;
+            }
+            Boolean token = result.result().bodyAsJsonObject().getBoolean("token");
+            if (!token) {
+                promise.fail("token invalid");
+                return;
+            }
+            System.out.println(result.result().body());
+            User user = UserKt.toUser(result.result().bodyAsJsonObject().getJsonObject("userRole"));
+            String username = user.getUsername();
+            FileSystem fs = vertx.fileSystem();
+            fs.open(filename, new OpenOptions(), fileRes -> {
+                if (fileRes.succeeded()) {
+                    System.out.println("/api/save/" + username + "/" + gameId);
+                    final MultipartForm form = MultipartForm.create();
+                    form.textFileUpload("txt-file", filename,path+filename,"text/plain");
+                    client
+                            .post("/api/save/" + username + "/" + gameId)
+                            .bearerTokenAuthentication(SusteamSdk.token)
+                            .sendMultipartForm(form, res -> {
+                                System.out.println(res.result().body());
+                                if (res.succeeded()) {
+                                    if( res.result().bodyAsJsonObject().getBoolean("success") ) {
+                                        promise.complete();
+                                    }
+                                    else {
+                                        promise.fail(res.result().bodyAsJsonObject().getString("error"));
+                                    }
+                                }
+                                else {
+                                    promise.fail(res.cause());
+                                }
+                            });
                 }
             });
-        } catch( Exception e ) {
-            promise.fail("File not found");
-        }
+        });
         return promise.future();
     }
 
 
-
-//    public static void main(String[] args) {
-//        SusteamSdk.init(
-//                "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VybmFtZSI6InRlc3QwMDEiLCJwZXJtaXNzaW9ucyI6W10sImlhdCI6MTYwMjE2NTU3M30.fGDVqNazElRUW6SXz11T9jZKAVJMn_7xTM49my7wfa0TY3bcVPD8Zd7mvmyUQKCH04Lj5xr5QD9Z00OWqxYXWHNjMnuXpHVJFQ0s2jL2XoY8CiyCn35JCKMQ20HD5hnSJAs9ACCp7aqSLxPklnBH4A_oD25U3_5MnSiEL7F8mWJIKrF0d5Dr82g47jPCBVyuKz4PNrFJhaiGEhQIRCfktZQ-g03PS02pD2D5GxJr_rcM7LWt_vYY3lwUPIlYj_c4NMwiLaKvGxA0jqFZicdIIGlvJRQ4yR48sWj9dQEj1PKFGgGcDUgVFJQkU4oKqLHwFlzXXUnfaEkXTwE7hAPZkQ",
-//                    10
-//        );
-//        SusteamSdk.isServerOnline().onComplete(it -> {
-//            if (it.succeeded()) {
-//                System.out.println("server is online");
-//            } else {
-//                System.out.println("server is not online");
+//    public static Future<Void> load() {
+//
+//        Promise<Void> promise = Promise.promise();
+//        HttpRequest<Buffer> request = client.get("/api/token").bearerTokenAuthentication(token);
+//        request.send(result -> {
+//            if (result.failed()) {
+//                promise.fail(result.cause());
+//                return;
 //            }
-//        });
+//            Boolean token = result.result().bodyAsJsonObject().getBoolean("token");
+//            if (!token) {
+//                promise.fail("token invalid");
+//                return;
+//            }
 //
-//        SusteamSdk.user().onSuccess(it -> {
-//            System.out.println(it.getUsername());
-//        });
+//            User user = UserKt.toUser(result.result().bodyAsJsonObject().getJsonObject("userRole"));
+//            String username = user.getUsername();
+//            String filename = username + "-" + gameId;
+//            System.out.println("/api/save/" + username + "/" + gameId);
+//            HttpRequest<Buffer> loadRequest = client.get("/api/save/" + username + "/" + gameId);
 //
-//        SusteamSdk.uploadSave("test.txt").onComplete(it -> {
-//           if (it.succeeded()) {
-//               System.out.println("upload success");
-//           } else {
-//               System.out.println(it.result());
-//           }
+//            loadRequest.send(res -> {
+//                if (res.failed()) {
+//                    promise.fail(res.cause());
+//                    return;
+//                }
+//                System.out.println(res.result().body());
+//            });
+//            promise.complete();
 //        });
+//        return promise.future();
 //    }
+
+
+
+    public static void main(String[] args) {
+        SusteamSdk.init("eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VybmFtZSI6InRlc3QwMDEiLCJwZXJtaXNzaW9ucyI6W10sImlhdCI6MTYwNTcwNzIxNn0.jo9VGmhssPLcKBvU2RfQOGTIsPnd1g-t5LD2ZI-ftqmEBJY06I0a5_kXN1Qc31AoUSwDNEp3JLY0Xku0-faw1DQGOSUUJLKf2wnvzY-36ZoGgVDgZEVgwfKuTyGL-uLuJevV3o4CBpcWx4XdJ0sbogx2oAszV1MR6n7bvSyIjPu368-cdRK4qZ_5Yrk9vfb88D8bH8SGR7AC7JINZam7YnFenk-0DDRDztYaQCgQn356Fz29Lzke3DOXw7gSQm1KPP2MQVJrCkUuZdPckl9PCCN7lj8xm8RM0C0H8B7ozp22qHhztqbcBRW0hXtycSlQ3k-QjdTv5P31_pZGwF7TxQ", 10);
+
+        SusteamSdk.isServerOnline().onComplete(it -> {
+            if (it.succeeded()) {
+                System.out.println("server is online");
+            } else {
+                System.out.println("server is not online");
+            }
+        });
+
+        SusteamSdk.user().onSuccess(it -> {
+            System.out.println(it.getUsername());
+        });
+        SusteamSdk.getGame(10);
+        SusteamSdk.save("C:\\Users\\yinpe\\IdeaProjects\\SUSTeam-SDK\\src\\","testfile.txt")
+                .onComplete(it -> {
+                    System.out.println("success");
+                });
+    }
 
 
 }

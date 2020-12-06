629f10f4d7c07c0ec121eff0640d5631b22c8b7d
dateri <263765432@qq.com>
2020-12-06 16:03:58 +0800

    Achievement class in SDK

---
+++ src/main/kotlin/susteam/sdk/Achievement.kt
@@ -0,0 +1,43 @@
+package susteam.sdk
+
+import io.vertx.core.json.JsonObject
+import io.vertx.kotlin.core.json.jsonObjectOf
+
+data class Achievement(
+        val gameId: Int,
+        val achievementId: Int,
+        val achievementName: String,
+        val description: String,
+        val achieveCount: Int
+)
+
+fun Achievement.toJson(): JsonObject = jsonObjectOf(
+        "gameId" to gameId,
+        "achievementId" to achievementId,
+        "achievementName" to achievementName,
+        "description" to description,
+        "achieveCount" to achieveCount
+)
+
+
+data class UserAchievementProcess(
+        val username: String,
+        val achievementId: Int,
+        val rateOfProcess: Int,
+        val finished: Boolean
+)
+
+
+fun UserAchievementProcess.toJson(): JsonObject = jsonObjectOf(
+        "username" to username,
+        "achievementId" to achievementId,
+        "rateOfProcess" to rateOfProcess,
+        "finished" to finished
+)
+
+fun JsonObject.toUserAchievementProcess(): UserAchievementProcess = UserAchievementProcess(
+        getString("username"),
+        getInteger("achievementId"),
+        getInteger("rateOfProcess"),
+        getBoolean("finished")
+)
